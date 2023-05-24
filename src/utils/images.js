ba14c4e26a8db0e9b26c673267c573169f6c987b
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-24 11:16:58 -0700

    update images from utils

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import slider_img_1 from "../assets/images/slider-img-1.png";
+import slider_img_2 from "../assets/images/slider-img-2.png";
+import slider_img_3 from "../assets/images/slider-img-3.png";
+import spinner from "../assets/images/spinner.svg";
+import error from "../assets/images/error.png";
 
-const images = () => {
-    return (
-        <div>images</div>
-    )
-}
-
-export default images
+const sliderImages = [slider_img_1, slider_img_2, slider_img_3];
+export { sliderImages, spinner, error };

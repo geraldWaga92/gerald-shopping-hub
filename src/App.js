39559ce0e9a72846277faa742b690703f122aa34
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-24 13:26:01 -0700

    wrap app.js with store provider

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ import { Home, Category, Cart } from "./pages/index";
 // components
 import Navbar from './components/Navbar/Navbar';
 import Footer from "./components/Footer/Footer";
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import store from "./store/store";
 
 function App() {
   return (
     <div className="App">
-      {/* <Provider store={store}> */}
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/category/:id" element={<Category />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-      {/* </Provider> */}
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/category/:id" element={<Category />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
